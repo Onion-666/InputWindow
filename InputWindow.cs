32fe780efbda7cd035aa61587a20b1046891f1a6
Халяпин Вадим <vadim.halyapin@yandex.ru>
2020-09-20 19:18:44 +0300

    Добавьте файлы проекта.

---
+++ InputWindow.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+class InputWindow
+{
+    static void Main()
+    {
+        string name;
+        name = Interaction.InputBox(
+            "Как вас зовут?",
+            "Давайте познакомимся");
+        string txt = "Очень приятно, " + name + "!";
+        MessageBox.Show(txt, "Знакомство состоялось");
+    }
+}
